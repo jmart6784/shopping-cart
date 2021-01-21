d02bdac7ca2b680352c46cd6f63c1a33d9fbf7cd
jmart6784 <jmartinez6784@yahoo.com>
2021-01-21 11:39:00 -0800

    Updates

---
+++ precache-manifest.59a51523ba6acdcda2a66ac2bec6a61a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9a88d89db215936ebd6f9487f7d8a2",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "6a6c4b4f8d3546f18d52",
+    "url": "/shopping-cart/static/js/2.861aea41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart/static/js/2.861aea41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398dc8b144bb65af1ed3",
+    "url": "/shopping-cart/static/js/main.68365b28.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  }
+]);
